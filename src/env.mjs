dbd9e993d2bf8ad3b47fb71152a70872e3362038
Damir Modyarov <otomir@yandex.ru>
2023-11-24 23:24:33 +0300

    feat: Create env variable for API URL

--- src/env.mjs
+++ src/env.mjs
@@ -2,6 +2,8 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-    server: {},
+    server: {
+        API_BASE_URL: z.string().url().default("https://new.akarpov.ru/api/v1/"),
+    },
     experimental__runtimeEnv: {}
 })
