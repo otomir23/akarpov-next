6be84099377763c5f255ec734331e0a04a3ac084
Damir Modyarov <otomir@yandex.ru>
2023-11-24 23:49:34 +0300

    feat: Setup media hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
-import "./src/env.mjs"
+import {env} from "./src/env.mjs"
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: env.MEDIA_BASE_HOSTNAME,
+            },
+        ],
+    },
+}
 
 export default nextConfig
--- src/env.mjs
+++ src/env.mjs
@@ -4,6 +4,7 @@ import { z } from "zod"
 export const env = createEnv({
     server: {
         API_BASE_URL: z.string().url().default("https://new.akarpov.ru/api/v1/"),
+        MEDIA_BASE_HOSTNAME: z.string().default("new.akarpov.ru"),
     },
     experimental__runtimeEnv: {}
 })
